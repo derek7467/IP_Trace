4270d98ea26af71d27231d49705eeb894b40a1c4
derek7467 <derek.napoli83@gmail.com>
2016-12-21 09:46:15 -0500

    Update IPTrace.py

--- IPTrace.py
+++ IPTrace.py
@@ -1,6 +1,4 @@
-Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:24:40) [MSC v.1500 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> #!/usr/bin/python
+#!/usr/bin/python
 
 import pygeoip
 import socket
